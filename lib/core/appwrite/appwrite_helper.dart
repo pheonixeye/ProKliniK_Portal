87153b151c4cf6339bf4ad562ce525bf8d81fc85
pheonixeye <drkazgm@gmail.com>
2024-07-02 00:01:43 +0300

    5th dep - dart-define

--- lib/core/appwrite/appwrite_helper.dart
+++ lib/core/appwrite/appwrite_helper.dart
@@ -8,17 +8,14 @@ class AppWriteHelper {
 
   static final Client _client = Client()
       .setEndpoint('https://cloud.appwrite.io/v1')
-      // .setProject(const String.fromEnvironment("project_id"));
-      .setProject("6682fa2900317f2c4745");
+      .setProject(const String.fromEnvironment("project_id"));
   late final Databases db;
 
   Future<void> sendFormSubmission(FormSubmission submission) async {
     try {
       await db.createDocument(
-        // databaseId: const String.fromEnvironment("database_id"),
-        databaseId: "6682fafa00222db75362",
-        // collectionId: const String.fromEnvironment("collection_id"),
-        collectionId: "6682fb1b0014f086e355",
+        databaseId: const String.fromEnvironment("database_id"),
+        collectionId: const String.fromEnvironment("collection_id"),
         documentId: ID.unique(),
         data: submission.toJson(),
       );
